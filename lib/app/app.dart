81f4040bd3db4a70518b06b3cbf5ea6bad97dbc8
matthewdauterman <matthew.dauterman@atomicrobot.com>
2025-02-27 11:02:20 -0500

    disabling flutter-gen localization

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,7 +4,6 @@ import 'package:carbon_flutter/providers.dart';
 import 'package:carbon_flutter/ui/design_system/theme.dart';
 import 'package:carbon_flutter/ui/navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,13 +17,19 @@ class App extends ConsumerWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
-        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: appConfiguration.supportedLocales,
       locale: appConfiguration.locale,
+      // localeResolutionCallback: (locale, supportedLocales) {
+      //   if (locale != null && supportedLocales.contains(Locale(locale.languageCode))) {
+      //     return locale;
+      //   } else {
+      //     return const Locale('en');
+      //   }
+      // },
       theme: appConfiguration.darkMode ? darkTheme : lightTheme,
       onGenerateTitle: (context) => context.localizations.appTitle,
       scaffoldMessengerKey: snackbarKey,
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Carbon Flutter';
+
+  @override
+  String get setting_dark_mode => 'Dark Mode';
+
+  @override
+  String get setting_language => 'Language';
+
+  @override
+  String get navigation_settings => 'Settings';
+
+  @override
+  String get navigation_design_system => 'Design System';
+}
